59c8665065525da501c4324b833088c017743e38
Gereksiz <40477096+GereksizPosta@users.noreply.github.com>
2020-12-10 23:43:42 -0500

    updates

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$pro=$_GET["pro"];
+$id=$_GET["id"];
+if ($id==null and $pro==null) {
+	$mod = 'steam_id';
+	$id='76561198070386645';
+} elseif ($pro==null) {
+	$id=$_GET["id"];
+	$mod = 'steam_id';
+} else {
+	$pro=$_GET["pro"];
+	$mod = 'profile_id';
+}
+?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -6,7 +20,7 @@
 	<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     <link rel="stylesheet" type="text/css" href="rank.css">
 </head>
-<body onload="ranks()">
+<body onload="ranks(<?php echo  "'" . $mod . "'" . ', ' . "'" . addslashes($id) . "'"; ?>)">
 <!-- <marquee behavior="scroll" direction="left" scrollamount="5"> -->
 	<div id="map">
 	</div>
