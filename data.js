dd29cf9baeb94e64b4bbe4b1190f920bf3e1773d
Gereksiz <40477096+GereksizPosta@users.noreply.github.com>
2021-01-27 12:02:27 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,63 @@
-function ranks(...args) {
-  var req = new XMLHttpRequest();
-  var url =
-    "https://aoe2.net/api/player/matches?game=aoe2de&count=1&" + args.join("=");
-  req.open("GET", url, true);
-  req.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(req.responseText);
-      renderHTML(data);
-    }
-  };
-  req.send();
+const color = [
+  {
+    id: 1,
+    string: '#405BFE',
+  },
+  {
+    id: 2,
+    string: '#FF0000',
+  },
+  {
+    id: 3,
+    string: '#00FF00',
+  },
+  {
+    id: 4,
+    string: '#FFFF00',
+  },
+  {
+    id: 5,
+    string: '#00FFFF',
+  },
+  {
+    id: 6,
+    string: '#FF57B3',
+  },
+  {
+    id: 7,
+    string: '#797979',
+  },
+  {
+    id: 8,
+    string: '#FF9600',
+  },
+];
+
+async function ranks(...args) {
+  const url =
+    'https://aoe2.net/api/player/matches?game=aoe2de&count=1&' + args.join('=');
+  const response = await fetch(url, {
+    mode: 'cors',
+  });
+  const data = await response.json();
+  renderHTML(data);
 }
 
 function renderHTML(data) {
-  var mapid = data[0].map_type;
-  var mapreq = new XMLHttpRequest();
-  var map_url =
-    "https://raw.githubusercontent.com/GereksizPosta/NightBotCommands/master/datafiles/aoe2.json";
-  mapreq.open("GET", map_url, true);
+  const mapid = data[0].map_type;
+  const mapreq = new XMLHttpRequest();
+  const map_url = 'https://aoe2.net/api/strings?game=aoe2de&language=en';
+  mapreq.open('GET', map_url, true);
   mapreq.onload = function () {
-    var mapdata = JSON.parse(mapreq.responseText);
+    const mapdata = JSON.parse(mapreq.responseText);
     for (i in mapdata.map_type) {
       if (mapdata.map_type[i].id == mapid) {
         mapname = mapdata.map_type[i].string;
       }
     }
-    document.getElementById("map").innerHTML = mapname;
-    var maxp = data[0].num_players;
-    var teams = [];
+    document.getElementById('map').innerHTML = mapname;
+    const maxp = data[0].num_players;
+    let teams = [];
 
     t = 0;
     while (t < maxp) {
@@ -36,32 +66,32 @@ function renderHTML(data) {
     }
     teams = [...new Set(teams)];
 
-    var bgimgurl = "https://www.countryflags.io/";
-    var bgimgend = "/flat/32.png";
+    const bgimgurl = 'https://www.countryflags.io/';
+    const bgimgend = '/flat/32.png';
 
-    var players = [];
+    let players = [];
 
     for (i = 0; i < maxp; i++) {
       for (k in mapdata.civ) {
-        if (mapdata.civ[k].id == data[0].players[i].civ) {
+        if (data[0].players[i].civ == mapdata.civ[k].id) {
           pciv = mapdata.civ[k].string;
         }
       }
-      for (c in mapdata.color) {
-        if (mapdata.color[c].id == data[0].players[i].color) {
-          pcolor = mapdata.color[c].string;
+      for (c in color) {
+        if (data[0].players[i].color == color[c].id) {
+          pcolor = color[c].string;
         }
       }
-      var pname = data[0].players[i].name.slice(0, 8);
-      var prating = data[0].players[i].rating;
+      const pname = data[0].players[i].name.slice(0, 8);
+      const prating = data[0].players[i].rating;
       if (data[0].players[i].country == null) {
-        var flag =
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/64px-International_Flag_of_Planet_Earth.svg.png";
+        flag =
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/International_Flag_of_Planet_Earth.svg/64px-International_Flag_of_Planet_Earth.svg.png';
       } else {
-        var flag = bgimgurl + data[0].players[i].country + bgimgend;
+        flag = bgimgurl + data[0].players[i].country + bgimgend;
       }
-      var pteam = data[0].players[i].team;
-      var pid = data[0].players[i].profile_id;
+      const pteam = data[0].players[i].team;
+      const pid = data[0].players[i].profile_id;
       players[i] = {
         name: pname,
         p_id: pid,
@@ -79,25 +109,25 @@ function renderHTML(data) {
     });
 
     for (t = 0; t < teams.length; t++) {
-      var name = "team" + teams[t];
-      newDiv = document.createElement("div");
-      newDiv.setAttribute("id", name);
-      document.getElementById("rank").appendChild(newDiv);
+      const name = 'team' + teams[t];
+      newDiv = document.createElement('div');
+      newDiv.setAttribute('id', name);
+      document.getElementById('rank').appendChild(newDiv);
 
       if (t < teams.length - 1) {
-        var vsDiv = document.createElement("div");
-        vsDiv.setAttribute("class", "vs");
+        const vsDiv = document.createElement('div');
+        vsDiv.setAttribute('class', 'vs');
         document.getElementById(name).after(vsDiv);
-        vsDiv.innerText = "VS";
+        vsDiv.innerText = 'VS';
       }
     }
 
     for (i = 0; i < players.length; i++) {
       const { country, civ, color, name, rating } = players[i];
-      var ht =
+      const ht =
         "<div class='wrap-player'><div class='player' style='background-image:url(" +
         country +
-        "), url(./civ_crests/" +
+        '), url(./civ_crests/' +
         civ +
         ".png); '><span class='name' style='color:" +
         color +
@@ -107,8 +137,8 @@ function renderHTML(data) {
         rating +
         "</span><br><span class='civ'>" +
         civ.slice(0, 9) +
-        "</span></div></div></div>";
-      var divn = "team" + players[i].team;
+        '</span></div></div></div>';
+      const divn = 'team' + players[i].team;
       document.getElementById(divn).innerHTML += ht;
     }
   };
