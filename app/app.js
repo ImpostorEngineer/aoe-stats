f6b9a8789ae69a588b7598861bfe80c222cc9179
ImpostorEngineer <posta.gereksiz@gmail.com>
2021-10-01 21:09:11 -0400

    fix civs

--- app/app.js
+++ app/app.js
@@ -14,19 +14,12 @@ router.get('/:id/:count', (req, res, next) => {
   }
 
   async function getGames() {
-    let civData = await axios.get(
-      'https://aoe2.net/api/strings?game=aoe2de&language=en'
-    );
+    let civData = await axios.get('https://aoe2.net/api/strings?game=aoe2de&language=en');
     const civNames = civData.data;
 
-    const url =
-      'https://aoe2.net/api/player/matches?game=aoe2de&' +
-      gamerID +
-      '&count=' +
-      count;
+    const url = 'https://aoe2.net/api/player/matches?game=aoe2de&' + gamerID + '&count=' + count;
 
     let response = await axios.get(url);
-
     let data = response.data;
 
     let playedGames = [];
@@ -34,18 +27,15 @@ router.get('/:id/:count', (req, res, next) => {
     let finalCivs = [];
 
     for (let i = 0; i < data.length; i++) {
-      const playerList = data[i].players.filter(
-        (player) => player[idType] == id
-      );
+      const playerList = data[i].players.filter((player) => player[idType] == id);
       playedGames.push(playerList);
       playedCivs.push(playerList[0].civ);
     }
 
     playedCivs = [...new Set(playedCivs)];
     playedCivs.sort((a, b) => a - b);
-
     for (let c = 0; c < playedCivs.length; c++) {
-      finalCivs.push(civNames.civ[playedCivs[c]]);
+      finalCivs.push(civNames.civ[playedCivs[c] - 1]);
       finalCivs[c].count = 0;
       finalCivs[c].won = 0;
     }
