9ce7e61de8989109f17b9f576bb7846b96d9103f
ImpostorEngineer <posta.gereksiz@gmail.com>
2021-10-21 23:47:22 -0400

    Added Winrate against an opponent

--- app/app.js
+++ app/app.js
@@ -55,4 +55,105 @@ router.get('/:id/:count', (req, res, next) => {
   getGames().then((response) => res.json(response));
 });
 
-module.exports = router;
+router.get('/vs/:id/:opponent', (req, res, next) => {
+  const opponent = req.params.opponent;
+  const id = req.params.id;
+  if (id.length > 7) {
+    idType = 'steam_id';
+    gamerID = 'steam_id=' + id;
+  } else {
+    idType = 'profile_id';
+    gamerID = 'profile_id=' + id;
+  }
+
+  async function getAllGames() {
+    const url = 'https://aoe2.net/api/player/matches?game=aoe2de&' + gamerID + '&count=1000';
+
+    let response = await axios.get(url);
+    let data = response.data;
+
+    let playedGames = [];
+    let winrate = {};
+    let count = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      const playerList = data[i].players.filter((player) => player[idType] == opponent);
+      if (playerList != '') {
+        playedGames.push(playerList);
+      }
+    }
+
+    for (let t = 0; t < playedGames.length; t++) {
+      if (playedGames[t][0].won == true) {
+        count += 1;
+      }
+    }
+
+    winrate.lost = count;
+    winrate.played = playedGames.length;
+    let opponentWinrate = Math.floor((count / playedGames.length) * 10000) / 100;
+    winrate.loserate = opponentWinrate + '%';
+    winrate.winrate = 100 - opponentWinrate + '%';
+
+    return winrate;
+  }
+  getAllGames().then((response) => res.json(response));
+});
+
+router.get('/current/:id/:count', (req, res, next) => {
+  const id = req.params.id;
+  const gameCount = req.params.count;
+  if (id.length > 7) {
+    idType = 'steam_id';
+    gamerID = 'steam_id=' + id;
+  } else {
+    idType = 'profile_id';
+    gamerID = 'profile_id=' + id;
+  }
+  const playerURL = 'https://aoe2.net/api/player/lastmatch?game=aoe2de&' + gamerID;
+
+  async function getCurrentGame() {
+    let currentGame = await axios.get(playerURL);
+
+    const currentPlayers = currentGame.data;
+    let opponent = {};
+
+    for (let p = 0; p < currentPlayers.last_match.players.length; p++) {
+      if (currentPlayers.last_match.players[p].profile_id != id) {
+        opponent.id = currentPlayers.last_match.players[p].profile_id;
+      }
+    }
+
+    const gameHistoryURL = 'https://aoe2.net/api/player/matches?game=aoe2de&' + gamerID + '&count=' + gameCount;
+
+    let response = await axios.get(gameHistoryURL);
+    let data = response.data;
+
+    let playedGames = [];
+    let playedCivs = [];
+    let winrate = {};
+    let count = 0;
+    for (let i = 0; i < data.length; i++) {
+      const playerList = data[i].players.filter((player) => player[idType] == opponent.id);
+      if (playerList != '') {
+        playedGames.push(playerList);
+      }
+    }
+
+    for (let t = 0; t < playedGames.length; t++) {
+      if (playedGames[t][0].won == true) {
+        count += 1;
+      }
+    }
+    winrate.lost = count;
+    winrate.played = playedGames.length;
+    let opponentWinrate = Math.floor((count / playedGames.length) * 10000) / 100;
+    winrate.loserate = opponentWinrate + '%';
+    winrate.winrate = 100 - opponentWinrate + '%';
+
+    return winrate;
+  }
+  getCurrentGame().then((response) => res.json(response));
+});
+
+https: module.exports = router;
